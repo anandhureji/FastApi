bde00e5014de72f496ef5d5effc3fb0ae55974d5
anandhu <anandhureji33@gmail.com>
2023-06-19 18:26:13 +0530

    get by id api added

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI,Depends
+from http.client import HTTPException
+
+
+
+from fastapi import FastAPI,Depends,Path,HTTPException
 from typing import Annotated
 
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
+from starlette import status
 
 import models
 from models import Todos
@@ -33,5 +38,15 @@ class TodoRequest(BaseModel):
 @app.get("/")
 async def read_all(db:db_dependency):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}",status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependency,todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
+
+
+
 
 
