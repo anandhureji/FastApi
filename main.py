c906f975cb015a33eebf8c1beb19cb5598723e3f
anandhu <anandhureji33@gmail.com>
2023-06-19 18:49:58 +0530

    added delete api

--- main.py
+++ main.py
@@ -65,6 +65,15 @@ async def update_todo(db:db_dependency,todo_id:int,todo_request:TodoRequest):
     db.commit()
 
 
+@app.delete("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db:db_dependency,todo_id:int):
+    todo_model = db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404,detail='Todo not found')
+    db.query(Todos).filter(Todos.id==todo_id).delete()
+    db.commit()
+
+
 
 
 
